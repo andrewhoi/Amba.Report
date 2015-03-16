366cc8aabd8bb4ebc647de661bcc4be94e6ccf4f
VladimirKazan <2330102@gmail.com>
2015-03-16 22:47:57 +0300

    add simple demo page

---
+++ Source/Amba.Report.Demo/Amba.Report.Demo/js/index.js
@@ -0,0 +1,99 @@
+﻿// #region
+//"2015-03-22T00:00:00"
+var json1 = {
+    "Date": new Date("2015-03-22T00:00:00"),
+    "Company": {
+        "Name": "Example company",
+        "Address": "Paris"
+    },
+    "PrintedAt": "2015-03-13T21:01:02",
+    "Categories": [
+      {
+          "Name": "Foods",
+          "Qnt": 100,
+          "Amount": 1000.0,
+          "Products": [
+            {
+                "Name": "Apple",
+                "Qnt": 40,
+                "Amount": 400.0,
+                "Orders": [
+                  {
+                      "Number": "2015-1",
+                      "Customer": "USA Goverment",
+                      "Qnt": 1,
+                      "Amount": 10.0
+                  },
+                  {
+                      "Number": "2015-2",
+                      "Customer": "John Hock",
+                      "Qnt": 29,
+                      "Amount": 290.0
+                  },
+                  {
+                      "Number": "2015-3",
+                      "Customer": "Adam Smith",
+                      "Qnt": 10,
+                      "Amount": 100.0
+                  }
+                ]
+            },
+            {
+                "Name": "Cherry",
+                "Qnt": 60,
+                "Amount": 600.0,
+                "Orders": [
+                  {
+                      "Number": "2015-1",
+                      "Customer": "USA Goverment",
+                      "Qnt": 1,
+                      "Amount": 599.99
+                  },
+                  {
+                      "Number": "2015-4",
+                      "Customer": "Jeam Bean",
+                      "Qnt": 59,
+                      "Amount": 0.01
+                  }
+                ]
+            }
+          ]
+      }
+    ]
+}
+
+// #endregion
+
+
+
+$(document).ready(function () {
+    $("#reportData").text(JSON.stringify(json1, null, '    '));
+
+    $('#submit').click(function () {
+        var formData = $("#reportData").val();
+
+        var $btn = $(this).button('loading');
+
+        $.ajax({
+            type: "POST",
+            url: "./api/report/Demo1",
+            data: formData,
+            dataType: "json",
+            contentType: "application/json"
+        })
+        .done(function (msg, status, jqxhr) {
+            // return url in Location header
+            var tempLocation = jqxhr.getResponseHeader("Location");
+            if (tempLocation) {
+                // download it
+                $("iframe[name='downloadFrame']").attr("src", tempLocation);
+            }
+        })
+        .always(function () {
+            $btn.button('reset');
+        });
+    });
+});
+
+
+
--- Source/Amba.Report.Demo/Startup.cs
+++ Source/Amba.Report.Demo/Startup.cs
@@ -15,7 +15,12 @@
 // along with "Amba.Report" library. If not, see <http://www.gnu.org/licenses/>
 
 using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
 using Owin;
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Web.Http;
 
 [assembly: OwinStartup(typeof(Amba.Report.Demo.Startup))]
@@ -30,15 +35,49 @@ namespace Amba.Report.Demo
 
             app.UseAmbaReport(config);
             app.UseWebApi(config);
-            
+
+
+            var staticOptions = new FileServerOptions()
+            {
+                RequestPath = PathString.Empty,
+                FileSystem = new PhysicalFileSystem(IsRunningInConsoleOwin ? @".\Amba.Report.Demo" : @".\bin\Amba.Report.Demo"), 
+                EnableDefaultFiles = true,
+            };
+#if DEBUG
+            if (IsRunningInConsoleOwin)
+            {
+                staticOptions.FileSystem = new PhysicalFileSystem(@"..\..\Amba.Report.Demo"); // Path to sources to edit html/js/css in IDE
+            }
+#endif
+            app.UseFileServer(staticOptions);
 
             app.Run(async (IOwinContext context) =>
             {
                 var bytes = System.Text.Encoding.UTF8
-                    .GetBytes("<h1>Hello, wolrd!</h1>");
+                    .GetBytes("404. Not found.");
+                context.Response.StatusCode = 404;
                 context.Response.ContentLength = bytes.Length;
                 await context.Response.WriteAsync(bytes);
             });
+        }
+
+        public static bool IsRunningInConsoleOwin
+        {
+            get
+            {
+                //Amba.Report.Demo.vshost.exe
+                var processName = Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName);
+                //c:\windows\system32\inetsrv\w3wp.exe for IIS
+                if (processName.Equals("Amba.Report.Demo.exe") ||
+                    processName.Equals("Amba.Report.Demo.vshost.exe"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
     }
 
